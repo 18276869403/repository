de4d0198fda431579a8861f37e5dab82061c1d3d
薄浩宇 <123456>
2020-05-23 16:45:17 +0800

    1

--- xcx_workman1/utils/configzpy.js
+++ xcx_workman1/utils/configzpy.js
@@ -1,8 +1,18 @@
 var api = require('./config.js')
-const baseUrl = "http://192.168.1.242:8081/work-boot/"
+//const baseUrl = "http://192.168.1.242:8081/work-boot/"
+const baseUrl = "http://192.168.1.250:8081/work-boot/"
 // 需求列表
-const xqurl = baseUrl + "work/need/list"
+const xqurl = baseUrl + "work/yneed/list"
+// 大厅最新需求
+const zuixinxq = baseUrl + "pc/hall/yneedList"
+// 推荐工人
+const tjgr = baseUrl + "pc/hall/yneedList"
+// 推荐商家
+const tjsj = baseUrl + "pc/hall/yneedList"
 
 module.exports = {
-  xqurl:xqurl
+  xqurl:xqurl,
+  zuixinxq:zuixinxq,
+  tjgr:tjgr,
+  tjsj:tjsj
 };
