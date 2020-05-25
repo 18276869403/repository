dee65d87954527e9d7bcb7fc3c2ae3ffbbc089d8
18276869403 <63405957+18276869403@users.noreply.github.com>
2020-05-25 14:31:36 +0800

    Add files via upload

--- xcx_workman1/pages/activity/activity.js
+++ xcx_workman1/pages/activity/activity.js
@@ -12,13 +12,38 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+
     this.setData({
       height: wx.getSystemInfoSync().windowHeight,　　//屏幕高度
       width: wx.getSystemInfoSync().windowWidth　　//屏幕宽度
-    
     })
     console.log(this.data.height)
   },
 
-  
+  getCode: function(){
+    wx.request({
+      url: 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx14e076d27e942480&secret=fb16e928e1a41fa0e8f21b2f50aa89d5',
+      method: 'get',
+      success: function (res) {
+        let data = {
+          scene: '123456', // 要传递的参数
+          page: 'pages/index/index', // 扫码进入的小程序页面
+        }
+    wx.request({
+          url: 'https://api.weixin.qq.com/wxa/getwxacodeunlimit?access_token='+res.data.access_token,
+          method: 'post',
+          data:data,
+          // dataType: 'json',
+          responseType: 'arraybuffer',         //将返回数据 按文本解析修改为arraybuffer
+          success: function (res) {
+            console.log()
+            self.setData({
+               //再arraybuffer转为base64  显示图片
+              url: 'data:image/png;base64,'+wx.arrayBufferToBase64(res.data)
+            })
+          }
+        })
+      }
+    })
+  }
 })
