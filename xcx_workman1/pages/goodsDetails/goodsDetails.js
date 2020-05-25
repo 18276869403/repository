34055510fa8a3e7cf19dfbb1c744e486bc1f7d3c
18276869403 <63405957+18276869403@users.noreply.github.com>
2020-05-25 14:29:23 +0800

    Add files via upload

--- xcx_workman1/pages/goodsDetails/goodsDetails.js
+++ xcx_workman1/pages/goodsDetails/goodsDetails.js
@@ -5,14 +5,19 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    spxqlist:[]
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // this.spxiangqinglist()
+  var splist = JSON.parse(options.obj)
+  console.log(splist);
+  this.setData({
+    splist: splist
+  })
   },
 
   /**
